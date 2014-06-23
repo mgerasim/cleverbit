84c451e1632d173e60476f037226cec2a20acb82
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-06-24 09:55:13 +1100

    CB0008 Added the git deployment script

---
+++ deploy.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /deploy.php was not found on this server.</p>
+<hr>
+<address>Apache/2.0.59 (Trustix Secure Linux/Linux) Server at <a href="mailto:you@your.address">server.com</a> Port 80</address>
+</body></html>
