9c0bd147dbc573e71b32e31d5ac734aaaaca16c1
Mikhail Gerasimov <mgerasim@inbox.ru>
2014-06-24 10:36:55 +1100

    CB0008 Added the git deployment script

--- deploy.php
+++ deploy.php
@@ -1,9 +1,50 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /deploy.php was not found on this server.</p>
-<hr>
-<address>Apache/2.0.59 (Trustix Secure Linux/Linux) Server at <a href="mailto:you@your.address">server.com</a> Port 80</address>
-</body></html>
+<?php
+/**
+* GIT DEPLOYMENT SCRIPT
+*
+* Used for automatically deploying websites via github or bitbucket, more deets here:
+*
+* https://gist.github.com/1809044
+*/
+ 
+// The commands
+$commands = array(
+'echo $PWD',
+'whoami',
+'git pull',
+'git status',
+'git submodule sync',
+'git submodule update',
+'git submodule status',
+);
+ 
+// Run the commands for output
+$output = '';
+foreach($commands AS $command){
+// Run it
+$tmp = shell_exec($command);
+// Output
+$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+$output .= htmlentities(trim($tmp)) . "\n";
+}
+ 
+// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+. ____ . ____________________________
+|/ \| | |
+[| <span style="color: #FF0000;">&hearts; &hearts;</span> |] | Git Deployment Script v0.1 |
+|___==___| / &copy; oodavid 2012 |
+|____________________________|
+ 
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
